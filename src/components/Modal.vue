d0e315c3faeb5278714edc962b7621cb382ce21f
Karl Erik Nelson <karlerik.nelson@tptlive.ee>
2024-09-28 14:08:22 +0300

    chars, colums jms

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,9 +6,7 @@ defineProps(['isOpen', 'url']);
     <div class="modal" :class="{'is-active':isOpen}">
           <div class="modal-background" @click="modalOpen=false"></div>
           <div class="modal-content">
-            <p class="image is-4by3">
-              <img src="url" alt="">
-            </p>
+              <slot></slot>
           </div>
           <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
         </div>
