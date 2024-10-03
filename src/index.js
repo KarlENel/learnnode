c736fc26088b9c1c54713021e6b43dc90e087fff
Karl Erik Nelson <karlerik.nelson@tptlive.ee>
2024-10-03 15:23:40 +0300

    Leaflet maps

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import Modals from './pages/Modals.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
 import NotFound from './pages/NotFound.vue';
 import RickandMorty from './pages/RickandMorty.vue';
+import Maps from './pages/Maps.vue';
 
 const routes = [
     { path: '/', name: 'ToDo', component: ToDo },
     { path: '/modals', name: 'Modals', component: Modals },
     { path: '/chuck', name: 'ChuckNorris', component: ChuckNorris },
     { path: '/RickandMorty', name: 'RickandMorty', component: RickandMorty },
+    { path: '/maps', name: 'Maps', component: Maps },
+    /** must always be last */
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound, meta: { showInTab: false }},
   ]
 
---
+++ src/pages/Maps.vue
@@ -0,0 +1,26 @@
+<script setup>
+import LeafletMap from '../components/LeafletMap.vue';
+import { ref } from 'vue';
+
+let map1Zoom = ref(19);
+let map1Lat = ref(59.42693);
+let map1Lng = ref(24.74350);
+
+</script>
+
+<template>
+    <input type="range" min="1" max="19" step="1" v-model="map1Zoom" > {{ map1Zoom }}
+    <button
+        class="button is-primary"
+        @click="map1Lat=40.71114;
+        map1Lng=-74.00242">Go to New York</button>
+    <LeafletMap
+        :lat="map1Lat"
+        :lng="map1Lng"
+        :zoom="map1Zoom"
+        @zoom="map1Zoom=$event$"
+        @move="map1Lat=$event.lat; map1Lng=$event.lng"></LeafletMap>
+    <LeafletMap lat="59" lng='24' zoom="7"></LeafletMap>
+    <LeafletMap lat="0" lng='0' zoom="12"></LeafletMap>
+
+</template>
